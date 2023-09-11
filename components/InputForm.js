c5804282271c4dd1f21404e5381a52e753459fe2
KimDoUk <zjaxj230@naver.com>
2023-09-11 17:08:17 +0900

    [design] InputForm css적용

--- components/InputForm.js
+++ components/InputForm.js
@@ -19,4 +19,44 @@ const InputForm = () => {
 
 export default InputForm
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    addFormContainer: {
+        flexDirection: 'row',
+        marginTop: 'auto',
+        marginBottom: 30,
+        paddingHorizontal: 20,
+        backgroundColor:'#f7f8fa'
+    },
+    inputField: {
+        flex: 1,
+        // backgroundColor:'red',
+        height: 42,
+        padding: 5,
+        marginRight: 25,
+        borderRadius: 4,
+        borderColor: 'rgba(0, 0, 0, 0.2)',
+        borderWidth: 1,
+        color:'#000000',
+        fontSize: 15,
+        textAlignVertical: 'center'
+    },
+    addButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 42,
+        height: 42,
+        borderRadius: 4,
+        backgroundColor:'rgba(0,0,0,0.7)',
+        shadowColor: '#000000',
+        shadowOpacity: 0.14,
+        shadowRadius: 8,
+        shadowOffset: {
+            width: 0,
+            height: 4
+        }
+    },
+    addButtonText: {
+        color: 'white',
+        fontSize: 25
+    }
+})
