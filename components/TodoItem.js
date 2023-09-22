b338ebf4bd8480a0e015e9e551ec9b2ebd10ee3c
KimDoUk <zjaxj230@naver.com>
2023-09-22 16:17:39 +0900

    [design] TodoItem css 적용

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -9,7 +9,7 @@ const TodoItem = () => {
     <View style={styles.itemContainer}>
       <Pressable
         hitSlop={10}
-        style={styles.itemTextChecked}
+        style={styles.itemCheckbox}
       >
         <CheckboxUnchecked/>
         <CheckboxChecked style={styles.itemCheckboxCheckedIcon}/>
@@ -34,5 +34,48 @@ const TodoItem = () => {
 
 export default TodoItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#f7f8fa'
+  },
+  itemCheckbox: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 20,
+      height: 20,
+      marginRight: 13,
+      borderRadius: 6
+  },
+  itemCheckboxCheckedIcon: {
+      shadowColor: '#000000',
+      shadowOpacity: 0.14,
+      shadowRadius: 8,
+      shadowOffset: {
+          width: 0,
+          height: 4
+      }
+  },
+  itemText: {
+      marginRight: 'auto',
+      paddingRight: 25,
+      fontSize: 15,
+      lineHeight: 20,
+      color: '#737373'
+  },
+  itemTextChecked: {
+      opacity: 0.3,
+      textDecorationLine: 'line-through'
+  },
+  deleteButton: {
+      opacity: 0.8
+  },
+  deleteButtonDone: {
+      opacity: 0.3
+  }
+})
 
