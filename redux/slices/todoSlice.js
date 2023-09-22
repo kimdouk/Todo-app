8252f6766d7ee80d2669ad5b6c25ffe707989e5b
KimDoUk <zjaxj230@naver.com>
2023-09-22 20:51:18 +0900

    [feat] redux reducer 생성

---
+++ redux/slices/todoSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+    name: 'todo',
+    initialState: {
+        currentId: 4,
+        todos: []
+    },
+    reducers: {}
+});
+
+export default todoSlice.reducer;
