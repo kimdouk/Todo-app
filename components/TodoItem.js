ff3999607550acd4196408956b6b0971a1a62895
KimDoUk <zjaxj230@naver.com>
2023-09-22 16:06:28 +0900

    [feat] TodoItem 컴포넌트 생성

---
+++ components/TodoItem.js
@@ -0,0 +1,38 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CheckboxUnchecked from '../assets/checkbox-unchecked.svg';
+import CheckboxChecked from '../assets/checkbox-checked.svg';
+import DeleteIcon from '../assets/delete.svg';
+
+const TodoItem = () => {
+  return (
+    <View style={styles.itemContainer}>
+      <Pressable
+        hitSlop={10}
+        style={styles.itemTextChecked}
+      >
+        <CheckboxUnchecked/>
+        <CheckboxChecked style={styles.itemCheckboxCheckedIcon}/>
+      </Pressable>
+      <Text
+        style={[styles.itemText, styles.itemTextChecked]}
+      >
+        코딩하기
+      </Text>
+      <Pressable
+        style={[
+          styles.deleteButton,
+          styles.deleteButtonDone
+        ]}
+        hitSlop={10}
+      >
+        <DeleteIcon/>
+      </Pressable>
+    </View>
+  )
+}
+
+export default TodoItem
+
+const styles = StyleSheet.create({})
+
