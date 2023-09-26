04c6fcd105e5e25b923192d1a33d33b6bb414595
KimDoUk <zjaxj230@naver.com>
2023-09-26 19:01:36 +0900

    [design] LoginScreen CSS 적용

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -44,5 +44,50 @@ const LoginScreen = () => {
 export default LoginScreen
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    inputContainer: {
+        width:'80%',
+        marginTop: 15,
+    },
+    input: {
+        backgroundColor:'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5
+
+    },
+    buttonContainer: {
+        width: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30
+    },
+    button: {
+        backgroundColor:'black',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    buttonOutline: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: 'black',
+        borderWidth: 1
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '500',
+        fontSize: 16
+    },
+    buttonOutlineText: {
+        color: 'black',
+        fontWeight: '500',
+        fontSize: 16
+    }
 })
