e182ebf23ca10681578c9bcd61c357c1ca06f228
KimDoUk <zjaxj230@naver.com>
2023-09-26 19:01:14 +0900

    [feat] Login Screen 생성

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import ListIcon from '../assets/list.svg';
 const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View
+        style={styles.container}
+    > 
+      <ListIcon/>
+      <View style={styles.inputContainer}>
+        <TextInput
+            placeholder='이메일'
+            value={email}
+            onChangeText={text => setEmail(text)}
+            style={styles.input}
+        />
+        <TextInput
+            placeholder='비밀번호'
+            value={password}
+            onChangeText={text => setPassword(text)}
+            style={styles.input}
+            secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+            style={styles.button}
+        >
+            <Text style={styles.buttonText}>로그인</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+            style={[styles.button, styles.buttonOutline]}
+        >
+            <Text style={styles.buttonOutlineText}>회원가입</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    
+})
