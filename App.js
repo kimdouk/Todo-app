a3a719a0db5cf8812157d8ed6549817b1f16c623
KimDoUk <zjaxj230@naver.com>
2023-09-26 17:51:29 +0900

    [feat] login/mainscreen 네비게이션 적용

--- App.js
+++ App.js
@@ -3,12 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 import MainScreen from './screens/MainScreen';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     // <MainScreen/>
       <Provider store={store}>
-        <MainScreen/>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen options={{ headerShown: false }} name="Main" component={MainScreen}/>
+            <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
   );
 }
